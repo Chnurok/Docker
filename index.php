7d4a65a919989deb5fa872565ee1082ca8598514
Chnurok <39839108+Chnurok@users.noreply.github.com>
2021-03-15 11:47:50 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $redis = new Redis();
-$redis->connect('127.0.0.1', 6379);
+$redis->connect('10.128.0.7', 6379);
 
 if ($_POST) {
     $num = $_POST["num"];
@@ -11,23 +11,26 @@ if ($_POST) {
         foreach ($mas as $value) {
             if ($value == $int_num){
               $b = False;
-              $res = array('error' => "the number has already arrived");
+              $res = array('error' => "the number has already arrived", 'server ip' => $_SERVER['SERVER_ADDR']);
               echo json_encode($res);
               break;
             }
             if ($value == $int_num + 1){
               $b = False;
-              $res = array('error' => "number is 1 less than already received");
+              $res = array('error' => "number is 1 less than already received", 'server ip' => $_SERVER['SERVER_ADDR']);
               echo json_encode($res);
               break;
             }
         }
         if ($b == True){
           $answer = $int_num+1;
-          $res = array('number' => $answer);
+          $res = array('number' => $answer, 'server ip' => $_SERVER['SERVER_ADDR']);
           echo json_encode($res);
           $redis->sAdd('arr', $int_num);
         }
-      }
+    } else {
+    	$res = array('error' => "string recieved", 'server ip' => $_SERVER['SERVER_ADDR']);
+        echo json_encode($res);
     }
+}
 ?>
